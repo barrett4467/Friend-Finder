4de8795986694344a512b450ac4f4ca5386ffa45
Rebecca Barrett <barrett4467@gmail.com>
2019-10-12 16:34:13 -0600

    Added comparison logic

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,5 +1,18 @@
-var friendsArray = [{
-    name: "jenny"
-}]
+
+var friendsArray = [
+    {
+        name: "Becca",
+        photo: "https://i.pinimg.com/originals/76/92/79/76927939ad6134c1b5b0fa472803ca4b.png",
+        scores: [
+            1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+        ]
+    }, {
+        name: "Test",
+        photo: "https://i.pinimg.com/originals/76/92/79/76927939ad6134c1b5b0fa472803ca4b.png",
+        scores: [
+            5, 5, 5, 5, 5, 5, 5, 5, 5, 5,
+        ]
+    }
+];
 
 module.exports = friendsArray;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -5,4 +5,51 @@ module.exports = function(app){
     app.get("/api/friends", function(req, res){
         res.json(friends);
     })
+
+    app.post("/api/friends", function(req, res) {
+          friends.push(req.body);
+          res.json(true);
+          var compatibility = [];
+          friends.forEach(function(friend, i){
+                var total = 0;
+                var newScores = req.body.scores;
+                var oldScores = friend.scores;
+                var newFriend = req.body.name;
+                var oldFriend = friend.name;
+
+
+                if (newFriend != oldFriend){
+                    console.log("Friend:");
+                    console.log("New: " + newFriend);
+                    console.log("Old: " + oldFriend);
+                    newScores.forEach(function(score){
+                        thisNewScore = parseInt(score);
+                    })
+                    oldScores.forEach(function(score){
+                        thisOldScore = parseInt(score);
+                    })
+                    total = Math.abs(thisNewScore - thisOldScore);
+                    console.log("this new: "+ thisNewScore);
+                    console.log("this old: "+ thisOldScore);
+    
+    
+                    console.log("Total Difference " + i + ": " + total);
+
+                } 
+                
+                compatibility.push({name: friend.name, difference: total});
+                
+                // console.log("NEW");
+                // console.log(newScores);
+                // console.log("OLD");
+                // console.log(oldScores);
+                // var compatibility = newScores - 
+            })
+            console.log(compatibility);
+            compatibility.forEach(function(score){
+                console.log(score.difference);
+            })
+
+      });
+    
 }
